c8505851a6d93e710448fd03857000bd9eb7756d
Alexander Gerhard <870717ag@gmail.com>
2026-06-20 13:12:53 +0200

    feat: migrate FinalBenefits

--- byggexp-lp-react/src/App.tsx
+++ byggexp-lp-react/src/App.tsx
@@ -1,6 +1,7 @@
 import BenefitsForOffice from "./components/Benefits/BenefitsForOffice"
 import BenefitsForSite from "./components/Benefits/BenefitsForSite"
 import Consequences from "./components/Consequences/Consequences"
+import FinalBenefits from "./components/FinalBenefits/FinalBenefits"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import How from "./components/How/How"
@@ -21,6 +22,7 @@ function App() {
      <BenefitsForOffice/>
      <BenefitsForSite/>
      <How/>
+     <FinalBenefits/>
     </>
   )
 }
